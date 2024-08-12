622454919e47f45061c88bff00e3c4c459ae71dd
leeeunda <daun0219@gmail.com>
2024-08-12 16:16:48 +0900

    feat: boiler plate code setting

---
+++ src/main/java/com/html/cifarm/intercepter/UserIdArgumentResolver.java
@@ -0,0 +1,36 @@
+package com.html.cifarm.intercepter;
+
+import com.html.cifarm.annotation.UserId;
+import com.html.cifarm.exception.CommonException;
+import com.html.cifarm.exception.ErrorCode;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class UserIdArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(Long.class)
+                && parameter.hasParameterAnnotation(UserId.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) {
+        final Object userIdObj = webRequest.getAttribute("USER_ID", WebRequest.SCOPE_REQUEST);
+
+        if (userIdObj == null) {
+            throw new CommonException(ErrorCode.INVALID_HEADER_ERROR);
+        }
+
+        return Long.valueOf(userIdObj.toString());
+    }
+}
---
+++ src/test/java/com/html/HTMLApplicationTests.java
@@ -0,0 +1,11 @@
+package com.html;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HTMLApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
