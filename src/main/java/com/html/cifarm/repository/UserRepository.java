7310cc668ca7ec062f0dc18db81c667068e6162a
leeeunda <daun0219@gmail.com>
2024-08-13 12:09:02 +0900

    fix: UserId 이름으로 인한 오류 수정

--- src/main/java/com/html/cifarm/repository/UserRepository.java
+++ src/main/java/com/html/cifarm/repository/UserRepository.java
@@ -16,14 +16,14 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByNickname(String nickname);
 
-    @Query("select u.userId as userId, u.role as role, u.password as password from User u where u.serialId = :serialId")
+    @Query("select u.id as id, u.role as role, u.password as password from User u where u.serialId = :serialId")
     Optional<UserSecurityForm> findSecurityFormBySerialId(String serialId);
 
-    @Query("select u.userId as userId, u.role as role, u.password as password from User u where u.userId = :id and u.isLogin = true")
+    @Query("select u.id as id, u.role as role, u.password as password from User u where u.id = :id and u.isLogin = true")
     Optional<UserSecurityForm> findSecurityFormById(Long id);
 
     @Modifying(clearAutomatically = true)
-    @Query("update User u set u.refreshToken = :refreshToken, u.isLogin = :isLogin where u.userId = :userId")
+    @Query("update User u set u.refreshToken = :refreshToken, u.isLogin = :isLogin where u.id = :userId")
     void updateRefreshTokenAndLoginStatus(Long userId, String refreshToken, Boolean isLogin);
 
     boolean existsByNickname(String nickname);
@@ -33,13 +33,13 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findBySerialId(String serialId);
 
     interface UserSecurityForm {
-        Long getUserId();
+        Long getId();
         ERole getRole();
         String getPassword();
         static UserSecurityForm invoke(User user) {
             return new UserSecurityForm() {
                 @Override
-                public Long getUserId() {
+                public Long getId() {
                     return user.getId();
                 }
 
