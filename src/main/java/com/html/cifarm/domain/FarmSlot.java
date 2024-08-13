0e8b9bb5abba67f88bf3f1b9eb42aee5434587bd
leeeunda <daun0219@gmail.com>
2024-08-13 18:58:11 +0900

    feat: FarmSlot 엔티티 수정

--- src/main/java/com/html/cifarm/domain/FarmSlot.java
+++ src/main/java/com/html/cifarm/domain/FarmSlot.java
@@ -1,16 +1,14 @@
 package com.html.cifarm.domain;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Setter
 @Table(name = "farm_slots")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FarmSlot {
@@ -23,22 +21,26 @@ public class FarmSlot {
     @JoinColumn(name = "farm_id", nullable = false)
     private Farm farm;
 
-    private Integer slotNumber;
+    private Integer availableSlotNumber;
 
     private Boolean isAvailable;
 
     private Integer slotPrice;
+
+    // 구획 총 개수가 아닌, 구획을 식별하는 번호
+    private Integer slotNumber;
 
     @CreatedDate
     @Column(name="created_at", nullable=false)
     private LocalDateTime createdAt;
 
     @Builder
-    public FarmSlot(Farm farm, Integer slotNumber, Boolean isAvailable, Integer slotPrice
+    public FarmSlot(Farm farm, Boolean isAvailable, Integer slotNumber, Integer availableSlotNumber, Integer slotPrice
     ) {
         this.farm=farm;
-        this.slotNumber = slotNumber;
         this.isAvailable = isAvailable;
+        this.slotNumber = slotNumber;
+        this.availableSlotNumber = availableSlotNumber;
         this.slotPrice = slotPrice;
         this.createdAt = LocalDateTime.now();
     }
---
+++ src/main/java/com/html/cifarm/repository/FarmSlotRepository.java
@@ -0,0 +1,7 @@
+package com.html.cifarm.repository;
+
+import com.html.cifarm.domain.FarmSlot;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FarmSlotRepository extends JpaRepository<FarmSlot, Long> {
+}
