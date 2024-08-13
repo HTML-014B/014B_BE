f6d5e1107f02c19bf609426a400e342a655f967c
leeeunda <daun0219@gmail.com>
2024-08-13 23:12:16 +0900

    feat: 농장 구획 선택하기 API

--- src/main/java/com/html/cifarm/repository/FarmUserRepository.java
+++ src/main/java/com/html/cifarm/repository/FarmUserRepository.java
@@ -1,7 +1,12 @@
 package com.html.cifarm.repository;
 
 import com.html.cifarm.domain.FarmUser;
+import com.html.cifarm.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface FarmUserRepository extends JpaRepository<FarmUser, Long> {
+    List<FarmUser> findByUser(User user);
 }
